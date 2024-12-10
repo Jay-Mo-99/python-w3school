29d7310ae62b694bd02dce66168586ec46a5246e
unknown <jaym991776@gmail.com>
2024-12-09 19:44:32 -0500

    02_variables: type,quote,case-sensitive

---
+++ 02_variables.py
@@ -0,0 +1,30 @@
+#Casting
+
+x=str("Black")
+y=int(500)
+z=float(4)
+
+# print(x)
+# print(y)
+# print(z)
+
+#Get the type
+# a = "apple"
+# b = 500
+
+# print(type(a))
+# print(type(b))
+
+#Quote
+x ="Jane"
+y='John'
+
+print(x)
+print(y)
+
+#Case-sensitive
+c = 500
+C ="White"
+
+print(c)
+print(C)

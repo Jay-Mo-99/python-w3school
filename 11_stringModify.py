ce0ae8cf1934c223e8244c99d1292d54c05da788
unknown <jaym991776@gmail.com>
2024-12-10 17:49:49 -0500

    11_stringModify

---
+++ 11_stringModify.py
@@ -0,0 +1,22 @@
+# a="I have a white space!"
+# print(a.split("a"))
+
+# b = "I have a white spcae"
+# print(b.upper())
+
+# c = "I HAVE A WHITE SPACE"
+# print(c.lower())
+
+# price = 100
+# txt = f"BMW, price: {price}"
+# print(txt)
+
+# txt = "BMW, price: "+  price
+# print(txt)
+
+
+price = 59
+txt = f"The price is {price:.3f} dollars"
+print(txt)
+txt= "\x48"
+print(txt)

f62948da8596eb485ed488d0971c8d06cd59f792
unknown <jaym991776@gmail.com>
2024-12-09 20:21:44 -0500

    04_assignValues: unpaking, assigning

---
+++ 04_assignValues.py
@@ -0,0 +1,22 @@
+# # x,y,z ="Apple", "Banana", "Cherry"
+
+# # print(x)
+# # print(y)
+# # print(z)
+
+# a=b=c="Durian"
+
+# print(a)
+# print(b)
+# print(c)
+
+# vege = ["Carrot","Onion","Mushrooms"]
+# e,d,f = vege
+# print(e)
+# print(d)
+# print(f)
+
+collection = (100,200,300)
+g,*h = collection
+
+print(h)
